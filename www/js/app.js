8c74c4195b27cea3c46c38c4b036661a6d2d7657
IgorSambonovich <igor_sambonovich@hotmail.com>
2016-10-25 16:17:14 -0200

    Header Shrink
    Fiz uma animação, pro cabeçalho desaparecer quando rolar pra baixo... Só
    testar depois. Ficou bem loco

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ionic.ion.headerShrink'])
 
  app.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
     $scope.toggleLeft = function() {
@@ -27,6 +27,8 @@ angular.module('starter', ['ionic'])
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
+      StatusBar.overlaysWebView(false);
+      StatusBar.backgroundColorByHexString('#387ef5');
     }
   });
 })
