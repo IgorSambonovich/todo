cc07485d52e14a07b0b728da4a85807b805eccea
IgorSambonovich <igor_sambonovich@hotmail.com>
2016-10-25 20:47:44 -0200

    Cagadas devidamente consertadas
    problema no app.js resolvido, e click no side menu funcionando

--- www/js/app.js
+++ www/js/app.js
@@ -3,16 +3,14 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','ngMockE2E','ionic.ion.headerShrink'])
+angular.module('starter', ['ionic','ionic.ion.headerShrink'])
 
- app.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
-    $scope.toggleLeft = function() {
-        $ionicSideMenuDelegate.toggleLeft()
+
+app.controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
+    $scope.abreEsq  = function() {
+    $ionicSideMenuDelegate.toggleLeft()
     }
 })
-
- 
-
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
